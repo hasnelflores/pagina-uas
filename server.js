3df73519c66147188ca05f4fd2ddd968d0a4a5c0
hansel flores <hanselcastrof@gmail.com>
2026-06-01 17:08:34 -0700

    Conexion Railway postgres

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+```javascript id="k5n7qc"
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
@@ -8,18 +11,19 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    user: 'citizix_user',
-    host: 'localhost',
-    database: 'citizix_db',
-    password: 'S3cret',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 app.get('/usuarios', async (req, res) => {
 
     try {
 
-        const result = await pool.query('SELECT NOW()');
+        const result = await pool.query(
+            'SELECT * FROM usuarios'
+        );
 
         res.json(result.rows);
 
@@ -35,8 +39,9 @@ app.get('/usuarios', async (req, res) => {
 
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
 
-    console.log('Servidor iniciado en puerto 3001');
+    console.log('Servidor iniciado');
 
 });
+```
